644349ce8ef44c5dddb0926d6844f8b6c0ebb1e5
jenniferyusiaa <arilhisyam247@gmail.com>
2024-01-16 21:21:17 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pro-prev-card-comp/",
   plugins: [react()],
-  base: "/pro-prev-card-comp",
 });
